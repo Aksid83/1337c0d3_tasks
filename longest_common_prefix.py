ba501eea6ff522ab82303d3f8a68bba2fcacedd6
Stan Sh <aksid83@gmail.com>
2019-12-22 15:42:47 -0800

    Added vertical and horisontal scan methods for the longest common prefix.

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -15,8 +15,13 @@ Note:
 All given inputs are in lowercase letters a-z.
 """
 
-# Not optimal solution :(
-def longestCommonPrefix(self, strs: list([str])) -> str:
+# Pythonic!
+def longestCommonPrefixPythonic(strs: list([str])) -> str:
+    import os
+    return os.path.commonprefix(strs)
+
+# Python zip() solution :(
+def longestCommonPrefix(strs: list([str])) -> str:
         pref = ""
         # Checking for empty list or list of empty strings
         if len(strs) == 0 or len(strs[0]) == 0: 
@@ -34,3 +39,31 @@ def longestCommonPrefix(self, strs: list([str])) -> str:
                 # If at least one letter is different, common prefix is done, exiting the loop
                 break
         return pref
+
+# Horisontal scanning method
+def longestCommonPrefixHorScan (strs: list([str])) -> str:
+    n = len(strs)
+    if n == 0 or len(strs[0]) == 0:
+        return ""
+    if n == 1:
+        return strs[0]
+    pref = strs[0]
+    for i in range(1, n):
+        while strs[i].find(pref) != 0:
+            pref = pref[:-1]
+            if len(pref) == 0: return ""
+    return pref
+
+# Vertical scanning method
+def longestCommonPrefixVertScan (strs: list([str])) -> str:
+    n = len(strs)
+    if n == 0 or len(strs[0]) == 0:
+        return ""
+    if n == 1:
+        return strs[0]
+    for i in range(len(strs[0])):
+        c = strs[0][i]
+        for j in range(1, n):
+            if i == len(strs[j]) or strs[j][i] != c:
+                return strs[0][:i]          
+    return strs[0]
